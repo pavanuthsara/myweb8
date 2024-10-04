3a1ea8e3d137ec00b99a8d57f68ef5b1161f3fd9
Pavan Uthsara <uthsarapavan@gmail.com>
2024-10-04 21:58:06 +0530

    add db connection

--- connection.php
+++ connection.php
@@ -1,15 +1,17 @@
 <?php 
 	
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = '';
-	$dbname = '';
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "apartmentdb";
 
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 	//checking the connection
-	if(mysqli_connect_errno()){
-		die('Database connection failed '. mysqli_connect_error());
-	} 
+	if (!$connection) {
+		die("Connection failed: " . mysqli_connect_error());
+	} else {
+		echo "Connected successfully!";
+	}
 
 ?>
